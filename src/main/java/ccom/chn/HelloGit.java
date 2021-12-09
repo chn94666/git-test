0bb21e18036017bb5cadd403feaed848c686c22e
chn <445365889@qq.com>
2021-12-09 14:54:42 +0800

    first commit

---
+++ src/main/java/ccom/chn/HelloGit.java
@@ -0,0 +1,15 @@
+package ccom.chn;
+
+/**
+ * @Author 蔡鸿宁
+ * @Date 2021/12/9 14:37
+ * @Version
+ * @description
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello git");
+    }
+}
