63af6f9175502edb896d0dd2e401ea0f11b2c458
chn94666 <95750324+chn94666@users.noreply.github.com>
2021-12-09 16:11:52 +0800

    Update HelloGit.java

--- src/main/java/ccom/chn/HelloGit.java
+++ src/main/java/ccom/chn/HelloGit.java
@@ -18,6 +18,7 @@ public class HelloGit {
         System.out.println("hello git master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 }
